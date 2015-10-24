e435e1c487a9f194529a4919ad968c357614b740
Hisham Ismail <mhishami@gmail.com>
2015-10-24 22:12:19 +0800

    Done for flow login, logout

--- src/auth_controller.erl
+++ src/auth_controller.erl
@@ -17,9 +17,15 @@ handle_request(<<"GET">>, <<"login">>, _Args, _Params, _Req) ->
 
 handle_request(<<"POST">>, <<"login">>, _Args, _Params, _Req) ->
 	{redirect, <<"/app">>};
-	
+
 handle_request(<<"GET">>, <<"signup">>, _Args, _Params, _Req) ->    
     {render, <<"signup">>, []};
+
+handle_request(<<"POST">>, <<"signup">>, _Args, _Params, _Req) ->    
+    {redirect, <<"/app">>};
+
+handle_request(<<"GET">>, <<"logout">>, _Args, _Params, _Req) ->    
+    {redirect, <<"/">>};
 
 handle_request(_, _, _, _, _) ->
     {error, <<"Opps, Forbidden">>}.
