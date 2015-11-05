2c848058ca1eae11800b1d2694508d549d25dbe4
Hisham Ismail <mhishami@gmail.com>
2015-11-06 02:27:11 +0800

    Donw with fetching messages

--- src/msg_controller.erl
+++ src/msg_controller.erl
@@ -2,7 +2,8 @@
 -export ([handle_request/5]).
 -include ("gruppz.hrl").
 
-handle_request(<<"GET">>, _Id, _Args, _Params, _Req) ->
-  ?DEBUG("Getting messages in msg_controller...~n", []),
-  {render, <<"forum_message">>, []}.
+handle_request(<<"GET">>, GroupId, [MsgId], _Params, _Req) ->
+  ?DEBUG("Getting messages for ~p, ~p ~n", [GroupId, MsgId]),
+  {ok, Post} = mongo_worker:find_one(?DB_POSTS, {<<"_id">>, MsgId, <<"grpid">>, GroupId}),
+  {render, <<"forum_message">>, [{post, Post}]}.
 
